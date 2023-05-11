eea3d7775d147cd67ad0754daf75cb1b2962de76
김채영 <cccodud@gmail.com>
2023-05-11 10:42:42 +0900

    Add: 복습을 위한 기본 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Counter() {
+    return (
+        <div>
+            <h1>Counter</h1>
+            <button>+</button> 👉 0
+            <table>
+                <thead>
+                    <tr>
+                        <td>time</td>
+                        <td>step</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
 }
 
 export default App;
